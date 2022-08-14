cb80e3bba4fe4793f9291ec7331df0b79e15086e
93287 <110309973+93287@users.noreply.github.com>
2022-08-14 12:42:20 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,105 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "I hope I haven't ...... you any trouble by changing the arrangements.",
+    answer: "caused",
+    options: [
+      "put",
+      "caused",
+      "made",
+      "done"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The floor is wet: don't run or you might ...... !",
+    answer: "slip",
+    options: [
+      "stoop",
+      "spill",
+      "slip",
+      "spin"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When you come to my house, ...... your camera with you.",
+    answer: "bring",
+    options: [
+      "take",
+      "show",
+      "fetch",
+      "bring"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Paul arrived at the shop ....... as the manager was closing for the day.",
+    answer: "just",
+    options: [
+      "even",
+      "just",
+      "still",
+      "right"
+    ]
+  },
+    {
+    numb: 5,
+    question: "I would ...... to stay at home and relax for a change.",
+    answer: "prefer",
+    options: [
+      "rather",
+      "better",
+      "prefer",
+      "enjoy"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Is there ...... of food for everyone?",
+    answer: "plenty",
+    options: [
+      "adequate",
+      "enough",
+      "sufficient",
+      "plenty"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Lily says she's happy at school but she's ...... complaining.",
+    answer: "always",
+    options: [
+      "rarely",
+      "sometimes",
+      "always",
+      "often"
+    ]
+  },
+  {
+    numb: 8,
+    question: "...... the step when you go in.",
+    answer: "Mind",
+    options: [
+      "Consider",
+      "Mind",
+      "Attend",
+      "Look"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
